a8aea0935883b60a74ccd79cae742b80c3de1bb6
riansyahrobi8 <robidahariansyah8@gmail.com>
2020-07-16 10:49:54 +0700

    belajar fittedbox pada image

--- lib/screens/ImageFittedBox.dart
+++ lib/screens/ImageFittedBox.dart
@@ -4,10 +4,12 @@ class ImageFittedBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      alignment: Alignment.topCenter,
+      color: Colors.amber,
       width: 500.0,
       height: 500.0,
-      child:
-          FittedBox(fit: BoxFit.cover, child: Image.asset('images/lake.jpg')),
+      child: FittedBox(
+          fit: BoxFit.fitWidth, child: Image.asset('images/lake.jpg')),
     );
   }
 }
