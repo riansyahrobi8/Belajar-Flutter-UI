ce1bfbe3b6361dec695fd6d049bc1111d452e5ec
riansyahrobi8 <robidahariansyah8@gmail.com>
2020-07-16 11:22:10 +0700

    hapus gambar

--- lib/screens/ContainerAspectRatio.dart
+++ lib/screens/ContainerAspectRatio.dart
@@ -4,12 +4,13 @@ class ContainerAspectRatio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 4 / 3,
+      aspectRatio: 16 / 9,
       child: Container(
           color: Colors.amber,
-          width: 200.0,
-          height: 100.0,
-          child: Image.asset('images/lake.jpg')),
+          child: Image.asset(
+            'images/lake.jpg',
+            fit: BoxFit.cover,
+          )),
     );
   }
 }
