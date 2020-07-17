8e89a6f2ef9f8133548ebfe7b500abd2a422d126
riansyahrobi8 <robidahariansyah8@gmail.com>
2020-07-17 12:51:34 +0700

    testing codemagic bagian ke 2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,14 +5,20 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:html';
+
 import 'package:belajar_ui_flutter/MyApp.dart';
+import 'package:belajar_ui_flutter/screens/TapLayout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(TapboxC(
+      active: true,
+      onChanged: (value) {},
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
