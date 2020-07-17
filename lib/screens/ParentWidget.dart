c455725e4174b7e91cfae5d634c239b060cb40c6
riansyahrobi8 <robidahariansyah8@gmail.com>
2020-07-17 09:56:05 +0700

    add layout tapboxb

---
+++ lib/screens/ParentWidget.dart
@@ -0,0 +1,27 @@
+import 'package:belajar_ui_flutter/screens/TapLayout.dart';
+import 'package:flutter/material.dart';
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
--- lib/screens/TapLayout.dart
+++ lib/screens/TapLayout.dart
@@ -35,3 +35,34 @@ class _TapboxAState extends State<TapboxA> {
     );
   }
 }
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+            color: active ? Colors.lightGreen[700] : Colors.grey[600]),
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
